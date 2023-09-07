d2ddf175aab42182269bb6a14b1ee815a1dabb8d
Chima <collins.chima.ilo@gmail.com>
2023-09-07 21:29:29 +0100

    utility functions

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { slug } from "github-slugger";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function slugify(str: string) {
+  return slug(str);
+}
+
+export function dateSort(a: string, b: string) {
+  if (a > b) return -1;
+  if (a < b) return 1;
+  return 0;
 }
